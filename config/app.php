ed46b07c675d55ae6a57466acebb719902202cb2
techoleg <semenikhinoleg777@yandex.ru>
2026-08-05 23:02:27 +0300

    fix: remove legacy Intervention Image registration

--- config/app.php
+++ config/app.php
@@ -194,11 +194,6 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
-
-
-        // ADDITIONAL
-        Intervention\Image\ImageServiceProvider::class,
-
     ],
 
     /*
@@ -214,7 +209,6 @@ return [
 
     'aliases' => Facade::defaultAliases()->merge([
         // 'ExampleClass' => App\Example\ExampleClass::class,
-        'Image' => Intervention\Image\Facades\Image::class
     ])->toArray(),
 
 ];
