932a8e4241fc2324c66b1d4f554e2e3224574cf8
techoleg <semenikhinoleg777@yandex.ru>
2026-08-05 22:26:59 +0300

    refactor: migrate Orchid role icons to Bootstrap

--- app/Orchid/Screens/Role/RoleEditScreen.php
+++ app/Orchid/Screens/Role/RoleEditScreen.php
@@ -76,11 +76,11 @@ class RoleEditScreen extends Screen
     {
         return [
             Button::make(__('Save'))
-                ->icon('check')
+                ->icon('bs.check-circle')
                 ->method('save'),
 
             Button::make(__('Remove'))
-                ->icon('trash')
+                ->icon('bs.trash')
                 ->method('remove')
                 ->canSee($this->role->exists),
         ];
