7cbc7f6df950f31548c244f38a9773ce7a6ed628
techoleg <semenikhinoleg777@yandex.ru>
2026-08-12 23:11:47 +0300

    fix: restore service center card content

--- resources/views/layouts/service-center-card.blade.php
+++ resources/views/layouts/service-center-card.blade.php
@@ -4,17 +4,39 @@
     @endif
     <div class="service-center-card__content">
         <div class="service-center-card__header">
+            <div class="service-center-card__logo">
+                @if (filled(trim((string) $serviceCenter->logo)))
+                    <img src="{{ $serviceCenter->logo . 'id/' . rand(1, 500) }}/100/100"
+                         alt="filter img" />
+                @endif
+            </div>
+
             <div class="service-center-card__info">
                 <h4 class="service-center-card__title">
-                    {{ $serviceCenter->name }}
+                    <a href="{{ route('service-centers.show', ['id' => $serviceCenter->id]) }}">
+                        {{ $serviceCenter->name }}
+                    </a>
                 </h4>
                 <x-display-rating rating="{{ $serviceCenter->average_rating }}"/>
                 <div class="service-center-card__data">
                     @if ($status = \App\Services\TitleService::timeBeforeClose($serviceCenter))
                         <p class="service-center-card__time">{!! $status !!}</p>
                     @endif
+                    @if (filled(trim((string) $serviceCenter->address)))
+                        <a class="service-center-card__address" href="#">
+                            {{ $serviceCenter->address }}
+                        </a>
+                    @endif
                 </div>
             </div>
+        </div>
+
+        <div class="service-center-card__footer">
+            @if (filled(trim((string) $serviceCenter->description)))
+                <p class="service-center-card__description">
+                    {{ $serviceCenter->description }}
+                </p>
+            @endif
         </div>
     </div>
 </div>
